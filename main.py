bf6f3523e6888246644eb6ad6bfdae2cbc2adffd
lyle <lilyui@126.com>
2018-05-16 10:07:11 +0800

    add setup

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ from handlers.tcp import tcpHandler
 
 if __name__ == '__main__':
     p = Pcap(sys.argv[1], [tcpHandler])
+    '''
+    for i in p.glob['tcpflow']:
+        i.extract()
+        for t in i.tcps:
+            print(repr(t))
+        for d in i.extract():
+            print(d)
+            print("=" * 50)
+    '''
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Trafficker",
+    version="1.0",
+    keywords=("Traffic", "Web"),
+    description="Tool For Parse Pcap",
+    license="MIT Licence",
+    author="lyle",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    package_data={},
+    scripts=[],
+)
