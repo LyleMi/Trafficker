0756bef4ee6319495066a456aa5bca81cb9a587a
lyle <lilyui@126.com>
2018-11-10 09:07:28 +0800

    add pcap filter

--- Trafficker/tools/pcapFilter.py
+++ Trafficker/tools/pcapFilter.py
@@ -4,20 +4,21 @@
 import sys
 
 from Trafficker.packets.pcap import Pcap
-from Trafficker.handlers.analysis import baseHandler
 
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("argument needed")
         return
-
+    whitelist = []
     p = Pcap(sys.argv[1])
-
+    newpcap = open(sys.argv[2], "wb")
+    newpcap.write(p.rawheader)
     for packetNum, packet in p.parse():
-        pass
-
-    p.parseWithCallback([baseHandler])
+        if packet.srcip in whitelist or packet.dstip in whitelist:
+            continue
+        newpcap.write(packet.raw)
+    newpcap.close()
 
 
 if __name__ == '__main__':
